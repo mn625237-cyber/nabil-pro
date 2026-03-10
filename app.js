1a6b8e3230917eabdd114d8aadf341a7f70a9dc0
mn625237-cyber <mn625237@gmail.com>
2026-03-10 02:16:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ function sanitize(str) {
 const RAILWAY_URL = 'https://nabil-pro-production.up.railway.app';
 
 const FIREBASE_CONFIG = {
-  apiKey:"AIzaSyAikfw9vS3PJQgaWl6SrpcOSG34B5vyXPc",
+  apiKey:"AIzaSyDpgMDU6DfzBK_TbqM0dARskUBdugMeDPA",
   authDomain:"nabil-pro.firebaseapp.com",
   projectId:"nabil-pro",
   storageBucket:"nabil-pro.firebasestorage.app",
